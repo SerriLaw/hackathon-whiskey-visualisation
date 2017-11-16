068aaec515fd960a2c818a72ab875ba6f3e8bac9
Sarah Lawrence <s.j.lawrence8@gmail.com>
2017-11-16 16:09:54 +1100

    header and footer

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,7 +3,24 @@ import './styles.css';
 
 const Header = () => (
 	<div id="pageTops" className="view">
-		<div className="main-text" />
+		<h1>Whiskey Visualisation</h1>
+		<h6>Whiskey - The cause of, and solution to, all life&#39;s problems</h6>
+		<nav>
+			<ul className="navigation">
+				<li>
+					<a href="">Graph 1</a>
+				</li>
+				<li>
+					<a href="">Graph 2</a>
+				</li>
+				<li>
+					<a href="">Graph 3</a>
+				</li>
+				<li>
+					<a href="">Graph 4</a>
+				</li>
+			</ul>
+		</nav>
 	</div>
 );
 export default Header;
--- src/pages/layout/index.js
+++ src/pages/layout/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import Header from '../../components/header';
+import Footer from '../../components/footer';
 import Router from '../router';
 import './styles.css';
 
 const Layout = () => (
 	<div id="wrapper">
 		<div id="inner_content">
+			<Header />
 			<BrowserRouter>
 				<Router />
 			</BrowserRouter>
+			<Footer />
 		</div>
 	</div>
 );
